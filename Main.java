3729cd2c5acf9e3fbfce87dafdd55766016f5f2b
Binoagustus <binoagustus@gmail.com>
2022-11-24 08:56:13 +0530

    rule1 minimum 8 char for password

--- Main.java
+++ Main.java
@@ -8,31 +8,38 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		UserValidation userObj = new UserValidation();
 		boolean loop = true;
-		while(loop) {
-			System.out.println("\n1.First Name \n2.LastName \n3.Email \n4.Phone Number \n5.Exit");
+
+		while (loop) {
+			System.out.println("\n1.First Name \n2.LastName \n3.Email \n4.Phone Number "
+					+ "\n5.Password Validation \n6.Exit");
 			System.out.println("Enter an option");
 			byte input = sc.nextByte();
-			switch(input) {
-				case 1:
-					userObj.checkFirstName();
-					break;
-					
-				case 2:	
-					userObj.checkLastName();
-					break;
-					
-				case 3:
-					userObj.emailValidation();
-					break;
-				
-				case 4:
-					userObj.phoneNumValidation();
-					break;
-					
-				case 5:
-					System.out.println("Terminated");
-					loop = false;
-					break;
+
+			switch (input) {
+			case 1:
+				userObj.checkFirstName();
+				break;
+
+			case 2:
+				userObj.checkLastName();
+				break;
+
+			case 3:
+				userObj.emailValidation();
+				break;
+
+			case 4:
+				userObj.phoneNumValidation();
+				break;
+
+			case 5:
+				userObj.passValidation();
+				break;
+
+			case 6:
+				System.out.println("Terminated");
+				loop = false;
+				break;
 			}
 		}
 	}
